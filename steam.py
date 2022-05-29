8bb0d7293ad4104013dcb06cbdafdd718709ba71
Serj Lubin <s.yu.lubin@gmail.com>
2022-05-29 21:14:04 +0300

    ФЫАФЫА

--- steam.py
+++ steam.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup 
 
 url = "https://www.akusherstvo.ru/events/bugaboo/kolyaski-bugaboo/"
@@ -7,11 +8,15 @@ requests = requests.get(url)
 # print(requests.text)
 
 soup = BeautifulSoup(requests.text , "html.parser")
-# print(soup, "line 9 is" )
-teme = soup.find_all("div", class_="itemInfo") 
-print(teme, "line 11 is")
-test= teme.find_all("p", class_="itemName")
-for href  in test.find_all("a" ):
+#print(soup.text , "line 9 is" )
+
+
+teme = soup.find_all(class_="itemName" ,  ) 
+print(teme , "line 11 is")
+print ()
+
+sys.exit(0)
+for href  in test.find_all("a"):
 	print (href, "line 14 is")
 	# for href_lv1 in 
 
